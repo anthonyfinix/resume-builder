bac66cd1a6f525059c0a726fdfe7a95e09b39332
unknown <Anthony Finix>
2025-08-10 12:18:25 +0530

    removed unused value

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Button from "./Button";
 import { Flex } from "./Flex";
 import { Bars, Print } from "./icons";
