bb2898bc6f1572570538c6166f033e1eae848810
Anthony <anthony.f@joola.in>
2026-01-01 14:46:05 +0530

    updated margin

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,7 +8,7 @@ const Content = () => {
     const handleResize = ()=>{
       if(!containerRef.current) return;
       const { offsetWidth, offsetHeight } = containerRef.current;
-      setScale(Math.min((offsetWidth - 40) / 794, ( offsetHeight - 40) / 1123));
+      setScale(Math.min((offsetWidth - 60) / 794, ( offsetHeight - 90) / 1123));
     }
     const observer = new ResizeObserver(handleResize)
     if(containerRef.current) observer.observe(containerRef.current)
