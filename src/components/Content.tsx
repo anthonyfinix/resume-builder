784628191abd63d89131025a44b916d31e13014c
Anthony <anthony.f@joola.in>
2026-01-01 14:19:08 +0530

    added auto resize

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,21 @@
+import { useLayoutEffect, useRef, useState } from 'react';
 import Modern from '../templates/Modern'
 
 const Content = () => {
-  return <div className="content">
-    <div style={{ transform: "scale(0.7)" }}>
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [scale, setScale] = useState(1);
+  useLayoutEffect(()=>{
+    const handleResize = ()=>{
+      if(!containerRef.current) return;
+      const { offsetWidth, offsetHeight } = containerRef.current;
+      setScale(Math.min((offsetWidth - 40) / 794, ( offsetHeight - 40) / 1123));
+    }
+    const observer = new ResizeObserver(handleResize)
+    if(containerRef.current) observer.observe(containerRef.current)
+    return () => observer.disconnect();
+  }, [])
+  return <div className="content" ref={containerRef}>
+    <div style={{ transform: `scale(${scale})` }}>
         <Modern />
     </div>
   </div>;
